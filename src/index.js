b8049e84a4b404142b015a5128b79e62adab6b9d
Balraj Johal <balrajsjohal@gmail.com>
2022-10-11 18:54:40 +0100

    give my grass a sexy gradient

--- src/index.js
+++ src/index.js
@@ -160,7 +160,12 @@ const fragmentShader = glslify(/* GLSL */ `
   varying vec2 vUv;
 
   void main () {
-    gl_FragColor = vec4(vec3(0.35, 0.5, 0), 1.0);
+    vec3 color1 = vec3(0.192, 0.431, 0.216);
+    vec3 color2 = vec3(0.247, 0.710, 0.294);
+  
+    vec3 color = mix(color1, color2, vUv.y);
+
+    gl_FragColor = vec4(color, 1.0);
   }
 `);
 
