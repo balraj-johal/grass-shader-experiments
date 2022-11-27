c60476a2b9f33afe516b75ca07d0384ba61907b7
Balraj Johal <balraj.johal@phntms.com>
2022-11-27 20:42:53 +0000

    Get random seed

--- src/utils/rand.js
+++ src/utils/rand.js
@@ -1,9 +1,13 @@
 import { alea } from "seedrandom";
 
+const generateRandomString = (length = 6) => {
+  return Math.random().toString(20).slice(2, length);
+};
+
 export const getSeed = () => {
   let seed = window.localStorage.getItem("SEED");
   if (!seed) {
-    seed = "NEWSEED";
+    seed = generateRandomString(10);
     window.localStorage.setItem("SEED", seed);
   }
   return seed;
